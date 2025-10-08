fba36e8dab98cf5fdc9538baa87086384eaf796c
KWasteds <kelvinjo2008@gmail.com>
2025-10-08 16:10:32 +0700

    Commit No. 1 – Create MapsActivity and location permission request

---
+++ app/src/main/java/com/example/lab_week_07_77881/MapsActivity.kt
@@ -0,0 +1,96 @@
+package com.example.lab_week_07_77881
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.lab_week_07_77881.databinding.ActivityMapsBinding
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import android.content.pm.PackageManager
+import android.location.Location
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    // If granted by the user, execute the necessary function
+                    getLastLocation()
+                } else {
+                    // If not granted, show a rationale dialog
+                    // A rationale dialog is used for a warning to the user that the app will
+                    showPermissionRationale {
+                        requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+                    }
+                }
+            }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        // OnMapReady is called when the map is ready to be used.
+        // The code below is used to check for the location permission for the map functionality to work.
+        // If it's not granted yet, then the rationale dialog will be brought up.
+        when {
+            hasLocationPermission() -> getLastLocation()
+            // shouldShowRequestPermissionRationale automatically checks if the user has denied the permission before.
+            // If it has, then the rationale dialog will be brought up.
+            shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION) -> {
+                showPermissionRationale {
+                    requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+                }
+            }
+            else -> requestPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun getLastLocation() {
+        Log.d("MapsActivity", "getLastLocation() called.")
+    }
+
+    private fun showPermissionRationale(positiveAction: () -> Unit) {
+        // Create a pop up alert dialog that's used to ask for the required permission again to the user.
+        AlertDialog.Builder(this)
+            .setTitle("Location permission")
+            .setMessage("This app will not work without knowing your current location")
+            .setPositiveButton(android.R.string.ok) { _, _ -> positiveAction() }
+            .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+    }
+
+    private fun hasLocationPermission() =
+        ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+}
